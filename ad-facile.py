f1bbfefd0d389482d68996bbaeeab4dccba80cc2
Chaddaï Fouché <chaddai.fouche@gmail.com>
2017-04-17 15:05:19 +0200

    Après séance du 10/04/2017

---
+++ ad-facile.py
@@ -0,0 +1,27 @@
+﻿import random
+import time
+
+def adfacile():
+    n1=random.randint(0,10)
+    n2=random.randint(0,10)
+    op= str(n1) + '+' + str(n2) + "="
+    res=n1+n2
+    return (op, res)
+
+def play_adfacile() :
+    pts = 0
+    nquestion=0
+    t=time.time()
+    while nquestion != 10 :
+                (op, res) = adfacile()
+                resultat = int(input(op))
+                if resultat == res :
+                    pts = pts + 1
+                    nquestion = nquestion + 1
+                else :
+                    pts = pts - 1
+                    nquestion = nquestion + 1
+    print("Vous avez " + str(pts) + " points")
+    print("Vous avez mis " + str(time.time()-t) + " seconde" )
+
+play_adfacile()
---
+++ st-int.py
@@ -0,0 +1,24 @@
+﻿# Créé par perigueux, le 20/03/2017 en Python 3.2
+pts = 0
+nquestion=0
+n1=0
+n2=0
+import random
+import time
+
+t=time.time()
+
+
+while nquestion != 10 :
+    n1=random.randint(0,100)
+    n2=random.randint(0,100)
+    n3=n1-n2
+    resultat = int(input( str(n1) + '-' + str(n2) + "="))
+    if resultat == n3 :
+        pts = pts + 1
+        nquestion = nquestion + 1
+    else :
+        pts = pts - 1
+        nquestion = nquestion + 1
+print("Vous avez " + str(pts) + " points")
+print("Vous avez mis " + str(time.time()-t + 1) + " seconde" )
